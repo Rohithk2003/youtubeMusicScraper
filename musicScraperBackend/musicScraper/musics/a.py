747b59976bba696487163821078dc52c4628a035
Rohith Krishnan <rohithk652@gmail.com>
2024-04-11 15:03:06 +0530

    fixed music not downloading and error after clicking search

--- musicScraperBackend/musicScraper/musics/a.py
+++
@@ -1,9 +0,0 @@
-import os
-s = os.listdir()
-with open("music.txt","w") as file:
-    for i in s:
-        if not i.endswith("py") or not i.endswith("txt"):
-            file.write(i)
-            file.write("\n")
-    file.close()
-    
